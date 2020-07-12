b4bcb42fc2ba9be173ac0b9fc68c539237f136f1
Bjørn Tore Håvie <bjoern.tore.haavie@pexip.com>
2020-07-12 23:57:20 +0200

    try to run full suite on travis

--- test-one.sh
+++ test-one.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 if rg -q '// Error' "$1" || ! rg -q '// expect:' "$1"; then
-	echo "1..0 # Skipped: Either no supported tests or one or more unsupported tests found" 
-	exit
+  echo "1..0 # Skipped: Either no supported tests or one or more unsupported tests found"
+  exit
 fi
 
 echo "1..1"
-diff -q <(< "$1" raku -ne 'say $0.Str when /"// expect: "(.+)/') <(raku Program.pl6 "$1") && echo "ok 1 - $1" || echo "not ok 1 - $1 mismatch"
+diff -q <(raku <"$1" -ne 'say $0.Str when /"// expect: "(.+)/') <(raku Program.pl6 "$1") && echo "ok 1 - $1" || echo "not ok 1 - $1 mismatch"
